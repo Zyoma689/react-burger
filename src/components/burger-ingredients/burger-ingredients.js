c95a35b101936adf75c1df3b58aff21d60ebe1f6
Artem Ivanov <aie426205@gmail.com>
2022-01-28 15:18:20 +0300

    feat: добавить заголовок и IngredientsTabs в BurgerIngredients

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -2,6 +2,7 @@ import React from "react";
 import { CONSTRUCTOR_TITLES } from "../../utils/constants";
 import IngredientsCardList from "../ingredients-card-list/ingredients-card-list";
 import burgerIngredientsStyles from "./burger-ingredients.module.css"
+import IngredientsTabs from "../ingredients-tabs/ingredients-tabs";
 
 
 export default function BurgerIngredients({ data }) {
@@ -10,13 +11,17 @@ export default function BurgerIngredients({ data }) {
   const main = data.filter((ingredient) => ingredient.type === 'main');
 
   return (
-    <div className={`${burgerIngredientsStyles.container} custom-scroll`}>
-      <p className="text text_type_main-medium">{CONSTRUCTOR_TITLES.BUN}</p>
-      <IngredientsCardList ingredients={bun}/>
-      <p className="text text_type_main-medium">{CONSTRUCTOR_TITLES.SAUCE}</p>
-      <IngredientsCardList ingredients={sauce}/>
-      <p className="text text_type_main-medium">{CONSTRUCTOR_TITLES.MAIN}</p>
-      <IngredientsCardList ingredients={main}/>
-    </div>
+    <section>
+      <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
+      <IngredientsTabs/>
+      <div className={`${burgerIngredientsStyles.container} custom-scroll mt-10`}>
+        <p className="text text_type_main-medium">{CONSTRUCTOR_TITLES.BUN}</p>
+        <IngredientsCardList ingredients={bun}/>
+        <p className="text text_type_main-medium">{CONSTRUCTOR_TITLES.SAUCE}</p>
+        <IngredientsCardList ingredients={sauce}/>
+        <p className="text text_type_main-medium">{CONSTRUCTOR_TITLES.MAIN}</p>
+        <IngredientsCardList ingredients={main}/>
+      </div>
+    </section>
   );
 }
